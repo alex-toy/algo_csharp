a7f725a84929e0fbf7d5435df826e9da43309e8c
Alessio REA <area@dtek.fr>
2022-04-30 08:02:48 +0200

    filter duplicates

--- algos/algos/Program.cs
+++ algos/algos/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void DisplayArray<T>(T[] array)
+{
+    foreach (var item in array) { Console.WriteLine(item); }
+}
+
+
+int[] FilterDuplicates(int[] data)
+{
+    List<int> result = new List<int>();
+    for (int i = 0; i < data.Length; i++) 
+    { 
+        if(!result.Contains(data[i])) result.Add(data[i]);
+    }
+    return result.ToArray();
+}
+
+int[] data = new int[] {1, 2, 3, 2, 3, 4};
+
+int[] filtered = FilterDuplicates(data);
+DisplayArray<int>(filtered);
