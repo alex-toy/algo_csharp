0051b7d440c02ee740df6454f6273adbca041d19
Alessio REA <area@dtek.fr>
2022-04-30 08:39:06 +0200

    find sum pairs

--- algos/algos/Program.cs
+++ algos/algos/Program.cs
@@ -3,7 +3,6 @@
     foreach (var item in array) { Console.WriteLine(item); }
 }
 
-
 int[] FilterDuplicates(int[] data)
 {
     List<int> result = new List<int>();
@@ -14,7 +13,60 @@ int[] FilterDuplicates(int[] data)
     return result.ToArray();
 }
 
+int[] FilterDuplicatesLinq(int[] data)
+{
+    return data.ToList().Distinct().ToArray();
+}
+
 int[] data = new int[] {1, 2, 3, 2, 3, 4};
 
-int[] filtered = FilterDuplicates(data);
+int[] filtered = FilterDuplicatesLinq(data);
 DisplayArray<int>(filtered);
+
+
+
+string[] FilterWordsLinq(string[] words, string letters)
+{
+    return words.ToList().Where(w => w.Any(w => letters.Contains(w))).ToArray();
+}
+
+string[] FilterWords(string[] words, string letters)
+{
+    List<string> result = new List<string>();
+    for (int i = 0; i < words.Length; i++)
+    {
+        for(int j = 0; j < letters.Length; j++)
+        {
+            if(words[i].Contains(letters[j]))
+            {
+                result.Add(words[i]);
+            }
+        }
+
+    }
+    return result.ToArray();
+}
+
+string[] words = new string[] { "the", "dog", "got", "a", "bone" };
+string letters = "ae";
+string[] filteredWords = FilterWords(words, letters);
+DisplayArray<string>(filteredWords);
+
+
+
+int[] FindSumPair(int[] numbers, int k)
+{
+    for(int i = 0; i < numbers.Length-1; i++)
+    {
+        for(int j = i+1; j<numbers.Length; j++)
+        {
+            if(numbers[i] + numbers[j] == k) return new int[] { i, j };   
+        }
+    }
+    return new int[]{ 0, 0};
+}
+
+int[] numbers = { 1, 5, 8, 1, 2 };
+int k = 25;
+int[] result = FindSumPair(numbers, k);
+DisplayArray<int>(result);
